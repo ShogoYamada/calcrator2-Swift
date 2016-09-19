d2d2f943a463d73919ecb7e2e2dda346f9aa0042
山田尚吾 <yamadashougo@yamadashougo-no-macbook-air.local>
2016-09-19 12:34:11 +0900

    Initial Commit

---
+++ calcrator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calcrator2
+//
+//  Created by 山田尚吾 on 2016/09/19.
+//  Copyright © 2016年 山田尚吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
